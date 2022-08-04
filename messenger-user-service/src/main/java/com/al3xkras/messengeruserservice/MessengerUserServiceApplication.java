dfcbbe8aa4a50d4e5850c3c446ae0cf6c5bc7b35
Alexander Krasovskiy <alexandr.krasovskiy@gmail.com>
2022-08-04 18:51:20 +0300

    Initial config; Add User service

---
+++ messenger-user-service/src/main/java/com/al3xkras/messengeruserservice/MessengerUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.al3xkras.messengeruserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerUserServiceApplication.class, args);
+	}
+
+}
---
+++ messenger-user-service/src/test/java/com/al3xkras/messengeruserservice/MessengerUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.al3xkras.messengeruserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
